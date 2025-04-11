71a7d346481025fd0e5a05cf399613abfdcc2ca9
KIM-NAMHYOUNG <kknh4869@naver.com>
2025-04-11 13:47:40 +0900

    3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class', // 🌙 다크모드 활성화 (class 기반)
   theme: {
     extend: {},
   },
