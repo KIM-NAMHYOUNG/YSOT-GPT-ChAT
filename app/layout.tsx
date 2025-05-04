b4addd06f96506a612191ccc30bb0e09ab285602
KIM-NAMHYOUNG <kknh4869@naver.com>
2025-05-04 16:27:20 +0900

    g

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
-'use client'
-
 import './globals.css'
 import { ThemeProvider } from '@/lib/theme-context'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className="bg-white text-black dark:bg-zinc-900 dark:text-white transition-colors duration-300">
+      <body>
         <ThemeProvider>
           {children}
         </ThemeProvider>
